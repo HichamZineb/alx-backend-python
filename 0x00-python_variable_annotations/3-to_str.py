a4dc6d41c40778dc9178060d364dc8daad264be6
HichamZineb <hichamelamine@hotmail.com>
2024-01-11 17:09:52 +0000

    newfile 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module with a type-annotated function to_str that
+takes a float as an argument and returns its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that returns the string representation of a given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
